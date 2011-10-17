c6c44db8d763dfc924eaac13f515fe6f064386d6
Mengqi Zong <zongmengqi@gmail.com>
2011-10-17 16:50:46 -0400

    eight-puzzle node-expand implemented, appending bug exists

--- 8-Puzzle/eight-puzzle.lisp
+++ 8-Puzzle/eight-puzzle.lisp
@@ -1,4 +1,3 @@
-;
 ; The 8-Puzzle Problem
 ;
 ; Author: Mengqi Zong
@@ -29,6 +28,7 @@
   (equal puzzle '(0 1 2 3 4 5 6 7 8)))
 
 (defun random-init-state (step)
+  "randomly initiate state, randomly move the gap 'step' steps"
   (cond
     ((equal step 0) *state*)
     (t (let* ((gap-loc (find-gap *state*))
@@ -40,6 +40,7 @@
 	 (random-init-state step)))))
 
 (defun find-gap (puzzle)
+  "find the location of zero"
   (loop for i from 0 to 8
      do (if (equal (nth i puzzle) 0)
 	    (return-from find-gap i)))
@@ -47,6 +48,36 @@
   (return-from find-gap NIL))
 
 (defun move-puzzle (puzzle i j)
+  "Swap the ith element and jth element of puzzle"
   (let ((i_val (nth i puzzle)) (j_val (nth j puzzle)))
     (setf (nth i puzzle) j_val)
     (setf (nth j puzzle) i_val)))
+
+(defun solve-puzzle ()
+  (let ((node (list *state* 0 NIL (list))))
+    node
+    (funcall #'expand-node node)
+    node))
+
+(defun expand-node (node)
+  "node = (state hval parent (child nodes))"
+  (let* ((state (first node))
+	 (gap-loc (find-gap state))
+	 (entry-num (nth gap-loc *adj-num*)))
+    (loop for i from 1 to entry-num
+	 do (funcall #'expand-child node i))))
+
+(defun expand-child (node i)
+  (let* ((state (first node))
+	 (gap-loc (find-gap state))
+	 (adj-entry (nth gap-loc *adj*))
+	 child child-state child-hval move)
+    (setf child-state (copy-list state))
+    (setf move (nth i adj-entry))
+    (move-puzzle child-state gap-loc move)
+    (setf child-hval 0)
+    (setf child (list child-state child-hval node (list)))
+    (append (fourth node) child)
+    (format t "move = ~a " move)
+    (format t "child ~a " child)
+    (format t "fn = ~a~%" (fourth node))))
