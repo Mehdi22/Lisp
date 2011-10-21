db0ede58afb86a49b6bb3a43872b7ac537a6369b
Mengqi Zong <zongmengqi@gmail.com>
2011-10-21 10:18:29 -0400

    Heuristic Manhattan distance implemented

--- 8-Puzzle/eight-puzzle.lisp
+++ 8-Puzzle/eight-puzzle.lisp
@@ -1,13 +1,15 @@
+; The Eight Puzzle Problem
 ;
 ; Author: Mengqi Zong
-;
 ; Email:  mz2326@columbia.edu
 ;
 
 ; Load queue.lisp first
 (require :queue)
 
-(defvar *adj*		; neighboors
+; *** Global variable declaration ***
+
+(defvar *adj*		; neighboors for each tile, first element is tile.
   '((0 1 3)		; tile 0
     (1 0 2 4)		; tile 1
     (2 1 5)		; tile 2
@@ -18,22 +20,37 @@
     (7 4 6 8)		; tile 7
     (8 5 7)))		; tile 8
 
-(defvar *adj-num*
+(defvar *adj-num*	; number of neighboors for each tile
   '(2 3 2 3 4 3 2 3 2))
+
+(defvar *distance*	; distance for each tile to reach the goal tile
+  '((0 1 2 1 2 3 2 3 4)	; tile 0
+    (1 0 1 2 1 2 3 2 3) ; tile 1
+    (2 1 0 3 2 1 4 3 2) ; tile 2
+    (1 2 3 0 1 2 1 2 3) ; tile 3
+    (2 1 2 1 0 1 2 1 2) ; tile 4
+    (3 2 1 2 1 0 3 2 1) ; tile 5
+    (2 3 4 1 2 3 0 1 2) ; tile 6
+    (3 2 3 2 1 2 1 0 1) ; tile 7
+    (4 3 2 3 2 1 2 1 0) ; tile 8
+    ))
 
 (defvar *nodes-expanded* 0)
 
+; *** main functions ***
+
+; state representation
 (defstruct node
-  (state nil)
+  (state nil)	; the state
   (gap-loc nil)	; the location of 0, the space
   (cost 0)	; g(n), the path cost
   (h-val 0)	; h(n), the heuristic value of current state
   (parent nil))	; parent node
 
+; If you do not want to randomnize the initial state, just call this
+; function like this: (solve-puzzle heurf state 0).
 (defun solve-puzzle (&optional (heurf #'misplaced-tiles)
 		     (initial-state '(0 1 2 3 4 5 6 7 8)) (step 30))
-  "If you do not want to randomnize the initial state, just call this
-   function like this: (solve-puzzle heurf state 0)."
   (let* ((start (init-node heurf initial-state))
 	 (fringe (make-q :enqueue #'enqueue-priority :key #'fn-a*)))
     (random-init-node heurf start step)
@@ -44,6 +61,7 @@
     (A-star fringe nil heurf)
     ))
 
+; Just A-star, like the graph-search.
 (defun A-star (fringe closed heurf)
   "Given a priority queue, pop a node to check if it is goal node.
    If not, insert the expanded nodes to the queue and call A-star
@@ -63,14 +81,8 @@
 		       (cons node closed) heurf))
 	  ))))
 
-(defun init-node (heurf puzzle-state)
-  "You have to init gap-loc and h-val for every new node, so write
-   a new function."
-  (let* ((zloc (find-gap puzzle-state))
-	 (node (make-node :state (copy-list puzzle-state) :gap-loc zloc)))
-    (setf (node-h-val node) (funcall heurf (node-state node)))
-    node))
-
+; Function expand calls function generate-child to generate each successor.
+; And at last return a list of successors.
 (defun expand (heurf puzzle)
   "generate a bunch of successors"
   (let* ((zloc (node-gap-loc puzzle))
@@ -107,7 +119,7 @@
 	      (format t "step: ~a~%" step))
     ))
 
-; Heuristic Function
+; *** Heuristic Function ***
 
 (defun misplaced-tiles (state)
   (let ((mis-num 0))
@@ -116,8 +128,32 @@
 		(setf mis-num (1+ mis-num))))
     mis-num))
 
-; Helper Function
+(defun manhattan-distance (puzzle-state)
+  (let ((state (copy-list puzzle-state))
+	(total-dst 0))
+    (loop for i from 0 to 8
+	 do (let ((num (elt state i)))
+	      (loop while (/= num i)
+		   do (let ((dst (elt (elt *distance* i) num)))
+			(setf (elt state i) (elt state num))
+		        (setf (elt state num) num)
+		        (setf total-dst (+ total-dst dst))
+			(setf num (elt state i))
+			))
+	      ))
+    total-dst))
 
+; *** Helper Function ***
+
+; Have to init gap-loc and h-val for every new node, so
+; write a new function.
+(defun init-node (heurf puzzle-state)
+  (let* ((zloc (find-gap puzzle-state))
+	 (node (make-node :state (copy-list puzzle-state) :gap-loc zloc)))
+    (setf (node-h-val node) (funcall heurf (node-state node)))
+    node))
+
+; At last, it  will correctly calculate the heuristic value
 (defun random-init-node (heurf puzzle step)
   "randomly initiate state, randomly move the gap 'step' steps"
   (cond
@@ -132,12 +168,15 @@
 	 (decf step)
 	 (random-init-node heurf puzzle step)))))
 
+; goal test
 (defun is-goal-state (puzzle)
   (equal puzzle '(0 1 2 3 4 5 6 7 8)))
 
+; same state test
 (defun is-same-state (s1 s2)
   (equal s1 s2))
 
+; find the location of zerp, or gap, or space.
 (defun find-gap (puzzle-state)
   "find the location of zero"
   (loop for i from 0 to 8
